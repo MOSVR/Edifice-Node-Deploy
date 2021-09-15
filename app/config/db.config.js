3d75d13c7e6954c8dff304afc50a1a8119a15ccc
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-09-15 18:00:10 +0530

    db added

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "us-cdbr-iron-east-02.cleardb.net",
-  USER: "b7e24378878xxx",
-  PASSWORD: "0200exxx",
-  DB: "heroku_7643ec736354xxx"
+  HOST: "us-cdbr-east-04.cleardb.com",
+  USER: "b61cd04cb15f6a",
+  PASSWORD: "7a2339c8",
+  DB: "heroku_b0bc7bea958ea4a"
 };
