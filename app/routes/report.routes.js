257be4cc91aea553ed3eacde0120a5dbfb1e7eb1
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-09-15 18:51:35 +0530

    removed

--- app/routes/report.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const reports = require("./../controllers/report.controller.js");
-
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    router.post("/report", reports.generate);
-
-}
